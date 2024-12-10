fa96e52f049c44371295faed104e3d01e88787df
Seo hyun Jeong <153349640+lifelo@users.noreply.github.com>
2024-12-10 20:47:14 +0900

    Add files via upload

---
+++ hw12_prj/blog/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    image = models.ImageField(upload_to='images/', blank=True, null=True)
+    file = models.FileField(upload_to='files/', blank=True, null=True)
+
+    def __str__(self):
+        return self.title
